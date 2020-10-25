8b7e59932283cbb3da41fce3f835e684030ddc5e
Mahdi Alilouche <alilouche.mahdi@gmail.com>
2020-10-25 20:51:45 +0100

    just br tag correction!

--- pages/imagine.vue
+++ pages/imagine.vue
@@ -17,81 +17,109 @@
     </section>
     <section class="ic-container section">
       <h1 class="big-title">Context</h1>
-      <div class="grid gap-10 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2">
+      <div
+        class="grid gap-10 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2"
+      >
         <div>
           <p class="text-base leading-7">
-            Depuis sa création en 2007, le collectif Cinéma Mémoire filme, documente et agit sur le terrain
-            de la création artistique ; accompagne par ses actions les changements de la société Algérienne
+            Depuis sa création en 2007, le collectif Cinéma Mémoire filme,
+            documente et agit sur le terrain de la création artistique ;
+            accompagne par ses actions les changements de la société Algérienne
             par le biais du cinéma documentaire.
-            <br></br>
-            Au fond de cet engagement se pose continuellement la question de notre relation par les images avec le passé.
-            Le cinéma est le miroir d’un pafys, il est l’un des reflets de ses représentations. Il aide à construire une mémoire,
-            à prendre conscience de ses identités,
-            à développer un esprit critique et à porter et construire un regard sur soi.
-              <br></br>
-            Dans un contexte d’absence d’industrie cinématographique en Algérie, le collectif s’est donné pour mission
-            d’accompagner par la formation à la création de films documentaires dans le cadre
-            d’ate- liers professionnels destinés à des candidat.e.s de toutes les régions d’Algérie.
-            Faire connaître les métiers du cinéma et encourager la création de ciné-clubs en milieu
-            associatif et scolaire à travers tout le territoire.
+            <br />
+            Au fond de cet engagement se pose continuellement la question de
+            notre relation par les images avec le passé. Le cinéma est le miroir
+            d’un pafys, il est l’un des reflets de ses représentations. Il aide
+            à construire une mémoire, à prendre conscience de ses identités, à
+            développer un esprit critique et à porter et construire un regard
+            sur soi.
+            <br />
+            Dans un contexte d’absence d’industrie cinématographique en Algérie,
+            le collectif s’est donné pour mission d’accompagner par la formation
+            à la création de films documentaires dans le cadre d’ate- liers
+            professionnels destinés à des candidat.e.s de toutes les régions
+            d’Algérie. Faire connaître les métiers du cinéma et encourager la
+            création de ciné-clubs en milieu associatif et scolaire à travers
+            tout le territoire.
           </p>
         </div>
         <div>
-          <div class="picture-container ">
-          <img class="picture" src="https://images.unsplash.com/photo-1595966646527-a6e7a1b99732?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80" alt="alt">
+          <div class="picture-container">
+            <img
+              class="picture"
+              src="https://images.unsplash.com/photo-1595966646527-a6e7a1b99732?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80"
+              alt="alt"
+            />
           </div>
         </div>
       </div>
     </section>
-     <section class="ic-container  section">
+    <section class="ic-container section">
       <h1 class="big-title">L’idée</h1>
       <div class="grid grid-cols-3">
         <div class="col-span-3">
           <p class="mb-24 leading-7 text-base">
-            Imagine est un projet qui permettra au collectif de revenir sur ce fonds pour le rendre accessible et utilisable pour
-            des nouvelles créations. Archive qui reflète à la fois l’histoire de Cinéma Mémoire en lien avec l’histoire du cinéma
-            documentaire Algérie et l’histoire des luttes, revendications, chan- gements et rêves de la société.
-             D’une manière concrète, visible et palpable, elle cristallisera le croisement entre ces dimensions.
-            <br></br>
-             Elle constituera alors une ressource riche pour les jeunes générations du pays pour lesquelles l’accès aux
-              images de leurs propre histoire est souvent verrouillée. En tant que mémoire de nos ex- périences,
-              ce fonds permettra également de capitaliser – afin de poursuivre – le travail de Cinéma Mémoire.
+            Imagine est un projet qui permettra au collectif de revenir sur ce
+            fonds pour le rendre accessible et utilisable pour des nouvelles
+            créations. Archive qui reflète à la fois l’histoire de Cinéma
+            Mémoire en lien avec l’histoire du cinéma documentaire Algérie et
+            l’histoire des luttes, revendications, chan- gements et rêves de la
+            société. D’une manière concrète, visible et palpable, elle
+            cristallisera le croisement entre ces dimensions.
+            <br />
+            Elle constituera alors une ressource riche pour les jeunes
+            générations du pays pour lesquelles l’accès aux images de leurs
+            propre histoire est souvent verrouillée. En tant que mémoire de nos
+            ex- périences, ce fonds permettra également de capitaliser – afin de
+            poursuivre – le travail de Cinéma Mémoire.
           </p>
         </div>
         <div></div>
       </div>
-      <div class="grid gap-20 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+      <div
+        class="grid gap-20 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2"
+      >
         <div>
           <h1 class="text-3xl font-bold font-display mb-10">Accès</h1>
-        <p class="mb-12 text-base">
-          La force du projet Imagine réside dans l’accès aux voies professionnelles et à un environnement
-          de travail d’innovation et d’échange. Ceci implique l’encouragement à s’approprier ces archives – forme visuelle
-          de l’histoire – pour continuer et inscrire dans le temps l’effort de la création de mémoires multiples de l’Algérie.
-        </p>
+          <p class="mb-12 text-base">
+            La force du projet Imagine réside dans l’accès aux voies
+            professionnelles et à un environnement de travail d’innovation et
+            d’échange. Ceci implique l’encouragement à s’approprier ces archives
+            – forme visuelle de l’histoire – pour continuer et inscrire dans le
+            temps l’effort de la création de mémoires multiples de l’Algérie.
+          </p>
         </div>
         <div>
-         <h1 class="text-3xl font-bold font-display mb-10">Transmission</h1>
-        <p class="mb-12 text-base">
-          Ce fonds constituera une base pour la transmission des compétences professionnelles dans le domaine
-           du cinéma documentaire par des ateliers de formation dans les métiers du cinéma et des master class.
-        </p>
+          <h1 class="text-3xl font-bold font-display mb-10">Transmission</h1>
+          <p class="mb-12 text-base">
+            Ce fonds constituera une base pour la transmission des compétences
+            professionnelles dans le domaine du cinéma documentaire par des
+            ateliers de formation dans les métiers du cinéma et des master
+            class.
+          </p>
         </div>
         <div>
-            <h1 class="text-3xl font-bold font-display mb-10">Réflexion</h1>
-        <p class="mb-12 text-base">
-          Il sera un point de départ pour des questionnements et croisements de regards multiples entre
-          chercheurs/chercheuses et professionnel.le.s de cinéma sur le statut de l’image dans la société et dans l’histoire.
-        </p>
+          <h1 class="text-3xl font-bold font-display mb-10">Réflexion</h1>
+          <p class="mb-12 text-base">
+            Il sera un point de départ pour des questionnements et croisements
+            de regards multiples entre chercheurs/chercheuses et
+            professionnel.le.s de cinéma sur le statut de l’image dans la
+            société et dans l’histoire.
+          </p>
         </div>
         <div>
-            <h1 class="text-3xl font-bold font-display mb-10">Création / Partage</h1>
-        <p class="mb-12 text-base">
-          Collectivement et en alternance entre travail en commun et travail autonome,
-           Imagine va créer et développer des nouvelles images et imaginations.
-           <br></br>
-           Ensemble, nous allons élaborer des compréhen- sions du présent basé sur des lectures
-           et relectures du passé à travers nos différentes approches disciplinaires et histoires subjectives.
-        </p>
+          <h1 class="text-3xl font-bold font-display mb-10">
+            Création / Partage
+          </h1>
+          <p class="mb-12 text-base">
+            Collectivement et en alternance entre travail en commun et travail
+            autonome, Imagine va créer et développer des nouvelles images et
+            imaginations.
+            <br />
+            Ensemble, nous allons élaborer des compréhen- sions du présent basé
+            sur des lectures et relectures du passé à travers nos différentes
+            approches disciplinaires et histoires subjectives.
+          </p>
         </div>
       </div>
     </section>
@@ -99,89 +127,113 @@
       <div class="big-title">
         <h1>Structure et Activité</h1>
       </div>
-        <div class="grid grid-cols-1 lg:grid-cols-9 gap-x-20 mt-20">
-      <div class="flex flex-col lg:col-span-4">
-        <h2 class="text-3xl font-bold font-display mb-16">Ateliers</h2>
-        <p class="text-base font-bold mb-12">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-          elementum tellus egestas aliquam.
-        </p>
-        <p class="mb-12 font-regular leading-8 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
-          eleifend erat sed tincidunt velit tortor ut. In egestas cras integer
-          neque, ornare volutpat et ut cursus. Diam nisl id est, cras non
-          placerat ac vitae. Egestas at tortor nec tempor tristique varius nam
-          at. Placerat amet leo ac ut. Sed vestibulum mollis fermentum dictumst
-          facilisis. Gravida habitasse sagittis ultrices sagittis montes natoque
-          turpis.
-        </p>
-        <a href="#" class="link link-primary">
-          En savoir plus
-          <client-only placeholder=">">
-            <ion-icon name="chevron-forward-outline" class="link-icon" />
-          </client-only>
-        </a>
-      </div>
-      <div
-        class="flex flex-row items-center justify-center gap-10 lg:col-span-5 mt-24 lg:mt-0"
-      >
-        <div class="flex flex-col justify-start gap-10">
-          <img src="/img/home-rect-1.png" alt="" />
-          <img src="/img/home-rect-3.png" class="pr-10" alt="" />
+      <div class="grid grid-cols-1 lg:grid-cols-9 gap-x-20 mt-20">
+        <div class="flex flex-col lg:col-span-4">
+          <h2 class="text-3xl font-bold font-display mb-16">Ateliers</h2>
+          <p class="text-base font-bold mb-12">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            nec elementum tellus egestas aliquam.
+          </p>
+          <p class="mb-12 font-regular leading-8 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
+            eleifend erat sed tincidunt velit tortor ut. In egestas cras integer
+            neque, ornare volutpat et ut cursus. Diam nisl id est, cras non
+            placerat ac vitae. Egestas at tortor nec tempor tristique varius nam
+            at. Placerat amet leo ac ut. Sed vestibulum mollis fermentum
+            dictumst facilisis. Gravida habitasse sagittis ultrices sagittis
+            montes natoque turpis.
+          </p>
+          <a href="#" class="link link-primary">
+            En savoir plus
+            <client-only placeholder=">">
+              <ion-icon name="chevron-forward-outline" class="link-icon" />
+            </client-only>
+          </a>
         </div>
-        <div class="flex flex-col justify-end gap-10">
-          <img src="/img/home-rect-2.png" alt="" />
-          <img src="/img/home-rect-1.png" alt="" />
+        <div
+          class="flex flex-row items-center justify-center gap-10 lg:col-span-5 mt-24 lg:mt-0"
+        >
+          <div class="flex flex-col justify-start gap-10">
+            <img src="/img/home-rect-1.png" alt="" />
+            <img src="/img/home-rect-3.png" class="pr-10" alt="" />
+          </div>
+          <div class="flex flex-col justify-end gap-10">
+            <img src="/img/home-rect-2.png" alt="" />
+            <img src="/img/home-rect-1.png" alt="" />
+          </div>
         </div>
       </div>
-    </div>
     </section>
     <section class="p-0 section">
-       <div class="">
-       <h2 class=" ic-container text-3xl font-bold font-display mb-16">Ateliers</h2>
-       <div class="cards-container">
-         <div class="card-wrapper">
-           <div class="card-cover">
-             <img src="https://images.unsplash.com/photo-1504275107627-0c2ba7a43dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="">
-           </div>
-           <span>
-             <h2 class="text-3xl font-bold font-display mt-5">Capture the moment</h2>
-             <p class="text-sm mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Diam quam massa imperdiet malesuada at. Volutpat justo morbi nunc. </p>
-          </span>
-         </div>
+      <div class="">
+        <h2 class="ic-container text-3xl font-bold font-display mb-16">
+          Ateliers
+        </h2>
+        <div class="cards-container">
+          <div class="card-wrapper">
+            <div class="card-cover">
+              <img
+                src="https://images.unsplash.com/photo-1504275107627-0c2ba7a43dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt=""
+              />
+            </div>
+            <span>
+              <h2 class="text-3xl font-bold font-display mt-5">
+                Capture the moment
+              </h2>
+              <p class="text-sm mt-1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam
+                quam massa imperdiet malesuada at. Volutpat justo morbi nunc.
+              </p>
+            </span>
+          </div>
           <a href="#" class="card-wrapper">
-           <div class="card-cover">
-             <img src="" alt="">
-           </div>
-           <span>
-             <h2 class="text-3xl font-bold font-display mt-5">Capture the moment</h2>
-             <p class="text-sm mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Diam quam massa imperdiet malesuada at. Volutpat justo morbi nunc. </p>
-          </span>
-         </a>
-            <a href="#" class="card-wrapper">
-           <div class="card-cover">
-             <img src="" alt="">
-           </div>
-           <span>
-             <h2 class="text-3xl font-bold font-display mt-5">Capture the moment</h2>
-             <p class="text-sm mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Diam quam massa imperdiet malesuada at. Volutpat justo morbi nunc. </p>
-          </span>
-         </a>
-       </div>
-    </div>
+            <div class="card-cover">
+              <img src="" alt="" />
+            </div>
+            <span>
+              <h2 class="text-3xl font-bold font-display mt-5">
+                Capture the moment
+              </h2>
+              <p class="text-sm mt-1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam
+                quam massa imperdiet malesuada at. Volutpat justo morbi nunc.
+              </p>
+            </span>
+          </a>
+          <a href="#" class="card-wrapper">
+            <div class="card-cover">
+              <img src="" alt="" />
+            </div>
+            <span>
+              <h2 class="text-3xl font-bold font-display mt-5">
+                Capture the moment
+              </h2>
+              <p class="text-sm mt-1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam
+                quam massa imperdiet malesuada at. Volutpat justo morbi nunc.
+              </p>
+            </span>
+          </a>
+        </div>
+      </div>
     </section>
-    <section class="ic-container section grid gap-20 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+    <section
+      class="ic-container section grid gap-20 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2"
+    >
       <div class="">
         <div class="picture-container">
-          <img class="picture" src="https://images.unsplash.com/photo-1557053819-aa6046add523?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="alt">
+          <img
+            class="picture"
+            src="https://images.unsplash.com/photo-1557053819-aa6046add523?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="alt"
+          />
         </div>
-
       </div>
       <div>
-          <h2 class="text-3xl font-bold font-display mb-16">Collection thématique</h2>
+        <h2 class="text-3xl font-bold font-display mb-16">
+          Collection thématique
+        </h2>
         <p class="text-base font-bold mb-12">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
           elementum tellus egestas aliquam.
@@ -201,29 +253,31 @@
       </div>
     </section>
     <section class="section">
-       <h2 class=" ic-container text-3xl font-bold font-display mb-16">Members</h2>
+      <h2 class="ic-container text-3xl font-bold font-display mb-16">
+        Members
+      </h2>
       <div class="grid md:grid-row-3 lg:grid-rows-3 gap-20 overflow-x-auto">
-       <div class="grid grid-flow-col gap-20 ">
+        <div class="grid grid-flow-col gap-20">
           <MemberCard />
           <MemberCard />
           <MemberCard />
           <MemberCard />
           <MemberCard />
-       </div>
-       <div class="grid grid-flow-col gap-20">
-         <MemberCard />
-          <MemberCard />
-          <MemberCard />
-            <MemberCard />
-            <MemberCard />
-       </div>
-       <div class="grid grid-flow-col gap-20">
-         <MemberCard />
+        </div>
+        <div class="grid grid-flow-col gap-20">
           <MemberCard />
           <MemberCard />
           <MemberCard />
           <MemberCard />
-       </div>
+          <MemberCard />
+        </div>
+        <div class="grid grid-flow-col gap-20">
+          <MemberCard />
+          <MemberCard />
+          <MemberCard />
+          <MemberCard />
+          <MemberCard />
+        </div>
       </div>
     </section>
   </div>
