4c7b913d69de3b48f6f7fb48d1cf860a46dde798
Mahdi Alilouche <alilouche.mahdi@gmail.com>
2020-10-25 20:04:36 +0100

    start collection thematique page

---
+++ components/card/PostCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="full-w-post-card">
+    <div class="img-cover">
+      <img src="/img/post-card-img.jpg" alt="title" />
+    </div>
+    <h2 class="title">Cinéma</h2>
+    <div class="short_desc">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+        inventore vitae hic earum provident. Quae adipisci repellat assumenda
+        minus debitis, iusto veritatis! Suscipit blanditiis, facilis ea vel
+        animi exercitationem itaque.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="postcss" scoped>
+.full-w-post-card {
+  @apply relative flex flex-row flex-no-wrap;
+}
+
+.img-cover {
+  @apply w-3/5 pr-6;
+}
+
+.img-cover img {
+  @apply min-w-full min-h-full h-auto object-cover;
+}
+
+.title {
+  @apply text-7xl font-extrabold leading-none;
+  @apply absolute transform origin-center;
+  top: 17%;
+  left: calc(3 / 5 * 100%);
+  --transform-translate-x: -50%;
+  --transform-translate-y: -50%;
+}
+
+.short_desc {
+  @apply w-2/5 text-lg absolute transform pl-6;
+  top: 67%;
+  left: calc(3 / 5 * 100%);
+  --transform-translate-y: -50%;
+}
+</style>
---
+++ pages/collection-thematique/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="push-from-header">
+    <div class="head-title-block">
+      <h1 class="pages-head-title">
+        Nous créons des
+        <span class="text-secondary">Collection thématique </span> percutantes
+        qui se démarquent...
+      </h1>
+
+      <p class="pages-head-subtitle">
+        Cinema imagine creates impactful Documentaries that help viewers ask the
+        right questiosn and engage in various important discutions.
+      </p>
+    </div>
+    <div class="ic-container flex flex-col">
+      <PostCard />
+    </div>
+  </div>
+</template>
+
+<script>
+import PostCard from '@/components/card/PostCard'
+export default {
+  components: {
+    PostCard
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.head-title-block {
+  @apply w-full mx-auto flex flex-col;
+}
+
+@screen lg {
+  .head-title-block {
+    @apply w-8/12;
+  }
+}
+</style>
