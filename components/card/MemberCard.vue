07ea364798843b22f4f9f6e0e487d0418f6b3cc7
aliben <nappy_boy.a@hotmail.fr>
2020-11-06 13:53:28 +0100

    Added imagin horizontal parallax

--- components/card/MemberCard.vue
+++ components/card/MemberCard.vue
@@ -5,10 +5,10 @@
       src="https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
       alt=""
     />
-    <span class="text-white p-10 z-10">
+    <span class="text-white p-6 z-10">
       <h2 class="text-xl font-bold">Ali Benabbes</h2>
-      <h3 class="font-regular text-base">Cinéaste et producteur</h3>
-      <p class="font-light">Membre du comité de pilotage et monteur</p>
+      <h3 class="font-regular text-sm pb-5">Cinéaste et producteur</h3>
+      <p class="text-sm font-light">Membre du comité de pilotage et monteur</p>
     </span>
   </div>
 </template>
@@ -31,5 +31,16 @@ export default {}
   position: absolute;
   object-fit: cover;
   object-position: center center;
+}
+@media only screen and (max-width: 600px) {
+  .mb-container {
+    width: 220px;
+    height: 305px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    background-color: burlywood;
+  }
 }
 </style>
